438980c723154a5f8780f5e34bf97562a24017a6
iamainow <iamainow@gmail.com>
2025-07-06 13:47:29 +0400

    *

--- routes/Program.cs
+++ routes/Program.cs
@@ -18,7 +18,7 @@ static void AdaptersPrint()
     Console.WriteLine();
 }
 
-static void RoutePrint()
+static void RoutePrintAll()
 {
     List<Ip4RouteEntry> routeTable = Ip4RouteTable.GetRouteTable();
 
@@ -30,62 +30,90 @@ static void RoutePrint()
     Console.WriteLine();
 }
 
-AdaptersPrint();
+static void RoutePrint(int interfaceIndex)
+{
+    List<Ip4RouteEntry> routeTable = Ip4RouteTable.GetRouteTable();
 
-RoutePrint();
+    Console.WriteLine("{0,18} {1,18} {2,18} {3,5} {4,8} ", "DestinationIP", "NetMask", "Gateway", "IF", "Metric");
+    foreach (Ip4RouteEntry entry in routeTable.Where(x => x.InterfaceIndex == interfaceIndex))
+    {
+        Console.WriteLine("{0,18} {1,18} {2,18} {3,5} {4,8} ", entry.DestinationIP, entry.SubnetMask, entry.GatewayIP, entry.InterfaceIndex, entry.Metric);
+    }
+    Console.WriteLine();
+}
+
+static Ip4RangeSet Simplify(Ip4RangeSet set, uint delta)
+{
+    LinkedList<Ip4Range> internalResult = new LinkedList<Ip4Range>(set);
+
+    bool shouldIterate = false;
+    do
+    {
+        shouldIterate = false;
+        LinkedListNode<Ip4Range>? current = internalResult.First;
+        while (current is not null && current.Next is not null)
+        {
+            var next = current.Next;
+            if ((uint)current.Value.LastAddress + delta >= (uint)next.Value.FirstAddress)
+            {
+                current.Value = new Ip4Range(current.Value.FirstAddress, next.Value.LastAddress);
+                internalResult.Remove(next);
+                shouldIterate = true;
+            }
+            else
+            {
+                current = current.Next;
+            }
+        }
+
+        current = internalResult.First;
+        while (current is not null)
+        {
+            if (current.Value.Count <= delta)
+            {
+                var prevCurrent = current;
+                current = current.Next;
+                internalResult.Remove(prevCurrent);
+                shouldIterate = true;
+            }
+            else
+            {
+                current = current.Next;
+            }
+        }
+    } while (shouldIterate);
+
+    Ip4RangeSet result = new();
+    foreach (var item in internalResult)
+    {
+        result = result.Union(item);
+    }
+
+    return result;
+}
+
+AdaptersPrint();
 
 var networkInterface = NetworkInterface.GetAllNetworkInterfaces()
     .Where(x => x.Name.Contains("AmneziaVPN", StringComparison.OrdinalIgnoreCase))
     .First();
 
+int networkInterfaceIndex = networkInterface.GetInterfaceIndex();
+
+IPAddress gatewayIp = networkInterface.GetPrimaryGateway() ?? throw new Exception("PrimaryGateway is null");
+
+RoutePrint(networkInterface.GetInterfaceIndex());
+
 List<Ip4RouteEntry> routeTable = Ip4RouteTable.GetRouteTable();
 
-var exampleRoute = new Ip4RouteCreateDto
-{
-    //string destination, string mask, int interfaceIndex, int metric
-    DestinationIP = IPAddress.Parse("10.158.8.10"),
-    SubnetMask = IPAddress.Parse("255.255.255.255"),
-    Metric = 2,
-    InterfaceIndex = networkInterface.GetInterfaceIndex(),
-    GatewayIP = networkInterface.GetPrimaryGateway() ?? throw new Exception("PrimaryGateway is null"),
-};
-
-var route = routeTable
-    .Where(x => x.DestinationIP.Equals(exampleRoute.DestinationIP))
-    .Where(x => x.SubnetMask.Equals(exampleRoute.SubnetMask))
-    .Where(x => x.InterfaceIndex.Equals(exampleRoute.InterfaceIndex))
-    .Where(x => x.GatewayIP.Equals(exampleRoute.GatewayIP))
-    .FirstOrDefault();
-
-if (route is not null)
-{
-    Ip4RouteTable.DeleteRoute(new Ip4RouteDeleteDto
-    {
-        DestinationIP = exampleRoute.DestinationIP,
-        SubnetMask = exampleRoute.SubnetMask,
-        InterfaceIndex = exampleRoute.InterfaceIndex,
-        GatewayIP = exampleRoute.GatewayIP,
-    });
-    Console.WriteLine("route deleted");
-}
-try
-{
-    Ip4RouteTable.CreateRoute(exampleRoute);
-    Console.WriteLine("route created");
-}
-catch (Exception exception)
-{
-    Console.WriteLine(exception.ToString());
-}
-
-
+int metric = 5;
 
 Console.WriteLine("------------------------------");
 Console.ReadKey();
 
 var lines = await File.ReadAllLinesAsync("ru.txt");
 List<Ip4Subnet> ruSubnets = new List<Ip4Subnet>();
-Ip4RangeSet ruRangeSet = new Ip4RangeSet();
+
 foreach (string line in lines)
 {
     if (!string.IsNullOrEmpty(line))
@@ -96,75 +124,59 @@ foreach (string line in lines)
 
         var subnet = new Ip4Subnet(ip, mask);
         ruSubnets.Add(subnet);
-        ruRangeSet = ruRangeSet.Union(subnet.ToIp4Range());
-
-        //Console.WriteLine($"{line,18} => {subnet.FirstAddress,15} {subnet.Mask.ToFullString(),15} or {subnet.FirstAddress,15}-{subnet.LastAddress,15}");
     }
 }
 
-Console.WriteLine("------------------------------");
-//Console.ReadKey();
-
-var allIpRangeSet = new Ip4RangeSet().Union(new Ip4Range(new Ip4Address(0x00000000), new Ip4Address(0xFFFFFFFF)));
-var nonRuIpRangeSet = allIpRangeSet.Except(ruRangeSet);
-
-//foreach (var item in nonRuIpRangeSet)
-//{
-//    Console.WriteLine($"{item.FirstAddress,15} - {item.LastAddress,15} {item.Count}");
-//}
-
-//Console.WriteLine(nonRuIpRangeSet);
-Console.WriteLine("------------------------------");
-//Console.ReadKey();
-
-const uint delta = 10000;
-
-LinkedList<Ip4Range> ips = new LinkedList<Ip4Range>(nonRuIpRangeSet);
-
-bool shouldShrink = false;
-do
+Ip4RangeSet ruIps = new Ip4RangeSet();
+foreach (var subnet in ruSubnets)
 {
-    shouldShrink = false;
-    LinkedListNode<Ip4Range>? current = ips.First;
-    while (current is not null && current.Next is not null)
-    {
-        var next = current.Next;
-        if ((uint)current.Value.LastAddress + delta >= (uint)next.Value.FirstAddress)
-        {
-            current.Value = new Ip4Range(current.Value.FirstAddress, next.Value.LastAddress);
-            ips.Remove(next);
-            shouldShrink = true;
-        }
-        else
-        {
-            current = current.Next;
-        }
-    }
-
-    current = ips.First;
-    while (current is not null)
-    {
-        if (current.Value.Count <= delta)
-        {
-            var prevCurrent = current;
-            current = current.Next;
-            ips.Remove(prevCurrent);
-            shouldShrink = true;
-        }
-        else
-        {
-            current = current.Next;
-        }
-    }
-} while (shouldShrink);
-
-var lessNonRuIpRangeSet = new Ip4RangeSet();
-foreach (var item in ips)
-{
-    lessNonRuIpRangeSet = lessNonRuIpRangeSet.Union(item);
+    ruIps = ruIps.Union(subnet.ToIp4Range());
 }
 
-foreach (var item in lessNonRuIpRangeSet)
+var nonRuIps = new Ip4RangeSet()
+    .Union(new Ip4Range(new Ip4Address(0x00000000), new Ip4Address(0xFFFFFFFF)))
+    .Except(ruIps);
+
+foreach (var item in nonRuIps)
 {
     Console.WriteLine($"{item.FirstAddress,15} - {item.LastAddress,15} {item.Count,10} => {string.Join(", ", item.ToSubnets())}");
+}
+
+Console.WriteLine("------------------------------");
+Console.ReadKey();
+
+try
+{
+    Ip4RouteTable.DeleteRoute(new Ip4RouteDeleteDto
+    {
+        DestinationIP = IPAddress.Parse("0.0.0.0"),
+        SubnetMask = IPAddress.Parse("0.0.0.0"),
+        InterfaceIndex = networkInterfaceIndex,
+        GatewayIP = gatewayIp,
+    });
+    Console.WriteLine("route deleted: 0.0.0.0/0");
+}
+catch (Exception exception)
+{
+    Console.WriteLine($"error deleting route 0.0.0.0/0: {exception.GetBaseException().Message}");
+}
+
+foreach (var subnet in nonRuIps.SelectMany(x => x.ToSubnets()))
+{
+    try
+    {
+        Ip4RouteTable.CreateRoute(new Ip4RouteCreateDto
+        {
+            DestinationIP = new IPAddress(subnet.FirstAddress.AsByteArray()),
+            SubnetMask = new IPAddress(subnet.Mask.AsByteArray()),
+            InterfaceIndex = networkInterfaceIndex,
+            GatewayIP = gatewayIp,
+            Metric = 5,
+        });
+        Console.WriteLine($"route created: {subnet}");
+    }
+    catch (Exception exception)
+    {
+        Console.WriteLine($"error creating route {subnet}: {exception.GetBaseException().Message}");
+    }
 }
