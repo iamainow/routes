06d8f6cf1a369dd07fd139f21fd9b452a7324f49
iamainow <iamainow@gmail.com>
2025-07-31 23:51:58 +0400

    *

--- routeTable/Ip4RouteEntry.cs
+++ routeTable/Ip4RouteEntry.cs
@@ -12,40 +12,5 @@ namespace NativeMethods
         public int ForwardProtocol { get; set; }
         public int ForwardAge { get; set; }
         public int Metric { get; set; }
-
-        public static implicit operator Ip4RouteCreateDto(Ip4RouteEntry entry)
-        {
-            ArgumentNullException.ThrowIfNull(entry);
-            return entry.ToIp4RouteCreateDto();
-        }
-
-        public static implicit operator Ip4RouteDeleteDto(Ip4RouteEntry entry)
-        {
-            ArgumentNullException.ThrowIfNull(entry);
-            return entry.ToIp4RouteDeleteDto();
-        }
-
-        public Ip4RouteCreateDto ToIp4RouteCreateDto()
-        {
-            return new Ip4RouteCreateDto
-            {
-                DestinationIP = DestinationIP,
-                SubnetMask = SubnetMask,
-                GatewayIP = GatewayIP,
-                InterfaceIndex = InterfaceIndex,
-                Metric = Metric,
-            };
-        }
-
-        public Ip4RouteDeleteDto ToIp4RouteDeleteDto()
-        {
-            return new Ip4RouteDeleteDto
-            {
-                DestinationIP = DestinationIP,
-                SubnetMask = SubnetMask,
-                GatewayIP = GatewayIP,
-                InterfaceIndex = InterfaceIndex,
-            };
-        }
     }
 }
