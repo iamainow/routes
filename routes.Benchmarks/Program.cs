d9314e3ad1ea53c7e72992dde5b92975940ee01e
iamainow <iamainow@gmail.com>
2025-11-16 02:56:41 +0400

    add benchmark

---
+++ routes.Benchmarks/Program.cs
@@ -0,0 +1,15 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Environments;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using routes.Benchmarks;
+
+var config = ManualConfig.Create(DefaultConfig.Instance)
+    .AddJob(Job.Default
+        .WithRuntime(CoreRuntime.Core90)
+        .WithId("net9.0"))
+    .AddJob(Job.Default
+        .WithRuntime(CoreRuntime.Core10_0)
+        .WithId("net10.0"));
+
+BenchmarkRunner.Run<Ip4RangeSetBenchmarks>(config);
