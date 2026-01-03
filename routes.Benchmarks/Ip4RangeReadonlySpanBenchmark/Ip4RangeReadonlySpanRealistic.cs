5bc5c13de9a650bc40d6a5ea68ec1a6a9c1691f1
iamainow <iamainow@gmail.com>
2026-01-04 00:07:43 +0400

    *

---
+++ routes.Benchmarks/Ip4RangeReadonlySpanBenchmark/Ip4RangeReadonlySpanRealistic.cs
@@ -0,0 +1,124 @@
+using BenchmarkDotNet.Attributes;
+using Ip4Parsers;
+using System.Runtime.InteropServices;
+
+namespace routes.Benchmarks.Ip4RangeReadonlySpanBenchmark;
+
+[Config(typeof(BenchmarkManualConfig))]
+public class Ip4RangeReadonlySpanRealistic
+{
+    private string _subnetsText = "";
+    private Ip4Subnet[] _subnets = [];
+    private Ip4Range[] _bogon = [];
+
+    [GlobalSetup]
+    public async Task GlobalSetup()
+    {
+        Random random = new();
+        _subnetsText = await FetchAndParseRuAggregatedZoneAsync();
+        _subnets = Ip4SubnetParser.GetSubnets(_subnetsText).ToArray();
+        _bogon =
+        [
+            Ip4Subnet.Parse("0.0.0.0/8"), // "This" network
+            Ip4Subnet.Parse("10.0.0.0/8"), // Private-use networks
+            Ip4Subnet.Parse("100.64.0.0/10"), // Carrier-grade NAT
+            Ip4Subnet.Parse("127.0.0.0/8"), // Loopback
+            Ip4Subnet.Parse("169.254.0.0/16"), // Link local
+            Ip4Subnet.Parse("172.16.0.0/12"), // Private-use networks
+            Ip4Subnet.Parse("192.0.0.0/24"), // IETF Protocol Assignments
+            Ip4Subnet.Parse("192.0.2.0/24"), // TEST-NET-1
+            Ip4Subnet.Parse("198.51.100.0/24"), // TEST-NET-2
+            Ip4Subnet.Parse("192.88.99.0/24"), // 6to4 Relay Anycast
+            Ip4Subnet.Parse("192.168.0.0/16"), // Private-use networks
+            Ip4Subnet.Parse("198.18.0.0/15"), // Benchmark testing
+            Ip4Subnet.Parse("203.0.113.0/24"), // TEST-NET-3
+            Ip4Subnet.Parse("224.0.0.0/4"), // Multicast
+            Ip4Subnet.Parse("240.0.0.0/4"), // Reserved for future use
+        ];
+    }
+
+    private static async Task<string> FetchAndParseRuAggregatedZoneAsync()
+    {
+        try
+        {
+            using var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(new Uri("https://www.ipdeny.com/ipblocks/data/aggregated/ru-aggregated.zone"));
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            return content;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error fetching from internet: {ex.Message}");
+            throw;
+        }
+    }
+
+    [Benchmark]
+    public int Ip4RangeReadonlySpan_Realistic()
+    {
+        var all = Ip4SubnetParser.GetRanges("0.0.0.0/0");
+        var ip = Ip4SubnetParser.GetRanges("1.2.3.4");
+
+        var subnets = Ip4SubnetParser.GetRanges(_subnetsText);
+
+        var result = new Ip4RangeReadonlySpan();
+
+        Span<Ip4Range> s0 = stackalloc Ip4Range[result.CalcUnionBuffer(all.Length)];
+        int l0 = result.Union(all, s0);
+        var result0 = new Ip4RangeReadonlySpan(s0[..l0]);
+
+        Span<Ip4Range> s1 = stackalloc Ip4Range[result0.CalcExceptBuffer(ip.Length)];
+        int l1 = result0.Except(ip, s1);
+        var result1 = new Ip4RangeReadonlySpan(s1[..l1]);
+
+        Span<Ip4Range> s2 = stackalloc Ip4Range[result1.CalcExceptBuffer(_bogon.Length)];
+        int l2 = result1.Except(_bogon, s2);
+        var result2 = new Ip4RangeReadonlySpan(s2[..l2]);
+
+        Span<Ip4Range> s3 = stackalloc Ip4Range[result1.CalcExceptBuffer(subnets.Length)];
+        int l3 = result2.Except(subnets, s3);
+        var result3 = new Ip4RangeReadonlySpan(s3[..l3]);
+
+        return result3.RangesCount;
+    }
+
+    [Benchmark]
+    public int Ip4RangeReadonlySpan_RealisticWithoutParser()
+    {
+        var all0 = Ip4Range.All;
+        var all = MemoryMarshal.CreateSpan(ref all0, 1);
+
+        var ip0 = new Ip4Address(1, 2, 3, 4).ToIp4Range();
+        var ip = MemoryMarshal.CreateSpan(ref ip0, 1);
+
+        ListStackAlloc<Ip4Range> subnetList = new(stackalloc Ip4Range[_subnets.Length]);
+        foreach (var subnet in _subnets)
+        {
+            subnetList.Add(subnet.ToIp4Range());
+        }
+
+        Span<Ip4Range> subnets = subnetList.AsSpan();
+
+        var result = new Ip4RangeReadonlySpan();
+
+        Span<Ip4Range> s0 = stackalloc Ip4Range[result.CalcUnionBuffer(all.Length)];
+        int l0 = result.Union(all, s0);
+        var result0 = new Ip4RangeReadonlySpan(s0[..l0]);
+
+        Span<Ip4Range> s1 = stackalloc Ip4Range[result0.CalcExceptBuffer(ip.Length)];
+        int l1 = result0.Except(ip, s1);
+        var result1 = new Ip4RangeReadonlySpan(s1[..l1]);
+
+        Span<Ip4Range> s2 = stackalloc Ip4Range[result1.CalcExceptBuffer(_bogon.Length)];
+        int l2 = result1.Except(_bogon, s2);
+        var result2 = new Ip4RangeReadonlySpan(s2[..l2]);
+
+        Span<Ip4Range> s3 = stackalloc Ip4Range[result1.CalcExceptBuffer(subnets.Length)];
+        int l3 = result2.Except(subnets, s3);
+        var result3 = new Ip4RangeReadonlySpan(s3[..l3]);
+
+        return result3.RangesCount;
+    }
+}
---
+++ routes.Benchmarks/Ip4RangeReadonlySpanBenchmark/Ip4RangeReadonlySpanUnionExcept.cs
@@ -0,0 +1,55 @@
+using BenchmarkDotNet.Attributes;
+using routes.Extensions;
+
+namespace routes.Benchmarks.Ip4RangeReadonlySpanBenchmark;
+
+[Config(typeof(BenchmarkManualConfig))]
+public class Ip4RangeReadonlySpanUnionExcept
+{
+    [Params(1_000)]
+    public int Count { get; set; }
+
+    [Params(10, 100, 1_000)]
+    public int SetSize { get; set; }
+
+    private List<Ip4Range[]> rangesArray_1 = [];
+    private List<Ip4Range[]> rangesArray_2 = [];
+
+    [GlobalSetup]
+    public async Task GlobalSetup()
+    {
+        Random random = new();
+        rangesArray_1 = Enumerable.Range(0, Count).Select(_ => Ip4RangeSet.Generate(SetSize, random)).Select(x => x.ToIp4Ranges()).ToList();
+        rangesArray_2 = Enumerable.Range(0, Count).Select(_ => Ip4RangeSet.Generate(SetSize, random)).Select(x => x.ToIp4Ranges()).ToList();
+    }
+
+    [Benchmark]
+    public int Ip4RangeSetStackAlloc_ctor_Union2_span()
+    {
+        Span<Ip4Range> span = stackalloc Ip4Range[SetSize * 2];
+        int result = 0;
+        for (int index = 0; index < Count; ++index)
+        {
+            var set = new Ip4RangeSetStackAlloc(span, rangesArray_1[index]);
+            set.Union2ModifySpan(rangesArray_2[index]);
+            result += set.RangesCount;
+        }
+
+        return result;
+    }
+
+    [Benchmark]
+    public int Ip4RangeSetStackAlloc_ctor_ExceptUnsorted_span()
+    {
+        Span<Ip4Range> span = stackalloc Ip4Range[SetSize * 2];
+        int result = 0;
+        for (int index = 0; index < Count; ++index)
+        {
+            var set = new Ip4RangeSetStackAlloc(span, rangesArray_1[index]);
+            set.ExceptModifySpan(rangesArray_2[index]);
+            result += set.RangesCount;
+        }
+
+        return result;
+    }
+}
